c8e35a5630d3be75cc2f546172b80a0d10a61b9c
alekseisanatov <alexsanatov@gmail.com>
2025-06-07 19:37:34 +0300

    fixes

--- src/scraper.js
+++ src/scraper.js
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-core');
 
 exports.ApartmentData = {
   price: Number,
@@ -16,18 +16,21 @@ exports.ApartmentData = {
 
 exports.scrapeBonavaApartments = async function () {
   const browser = await puppeteer.launch({
+    executablePath: process.env.CHROME_BIN || '/usr/bin/google-chrome',
     args: [
       '--no-sandbox',
       '--disable-setuid-sandbox',
       '--disable-dev-shm-usage',
       '--disable-accelerated-2d-canvas',
       '--disable-gpu',
+      '--window-size=1920x1080'
     ],
     headless: true
   });
 
   try {
     const page = await browser.newPage();
+    await page.setViewport({ width: 1920, height: 1080 });
     await page.goto("https://www.bonava.lv/dzivokli", {
       waitUntil: "networkidle0",
     });
