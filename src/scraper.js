902a6a522030b137ed2e7a5277bf4bb7d97d53dc
alekseisanatov <alexsanatov@gmail.com>
2025-06-07 20:38:39 +0300

    fixes

--- src/scraper.js
+++ src/scraper.js
@@ -35,7 +35,7 @@ exports.scrapeBonavaApartments = async function () {
     console.log('Launching browser with args:', launchArgs);
 
     const browser = await puppeteer.launch({
-      executablePath: '/usr/bin/chromium-browser',
+      executablePath: '/usr/bin/chromium',
       args: launchArgs,
       headless: 'new',
       ignoreHTTPSErrors: true
