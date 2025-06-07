1801e8a2ec3cfec979ed07320e07738e686c008e
alekseisanatov <alexsanatov@gmail.com>
2025-06-07 19:04:16 +0300

    added chromium

--- src/scraper.js
+++ src/scraper.js
@@ -17,10 +17,20 @@ exports.ApartmentData = {
 exports.scrapeBonavaApartments = async function () {
   const browser = await puppeteer.launch({
     headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--disable-gpu',
+      '--window-size=1920x1080',
+    ],
+    executablePath: process.env.CHROME_BIN || null
   });
 
   try {
     const page = await browser.newPage();
+    await page.setViewport({ width: 1920, height: 1080 });
     await page.goto("https://www.bonava.lv/dzivokli", {
       waitUntil: "networkidle0",
     });
